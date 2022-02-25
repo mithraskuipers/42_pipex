e13ead5eee600254510ec670a6ccb08a99581745
Mithras Kuipers <mikuiper@f0r4s11.codam.nl>
2022-02-25 13:28:08 +0100

    update

---
+++ inc/pipex.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pipex.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/11 15:11:52 by mikuiper      #+#    #+#                 */
+/*   Updated: 2022/02/11 16:42:12 by mikuiper      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#define MODE_INPUT 0
+#define MODE_OUTPUT 1
+
+#endif
--- src/pipex.c
+++ src/pipex.c
@@ -5,15 +5,66 @@
 /*                                                     +:+                    */
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/02/25 11:40:24 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/02/25 11:51:09 by mikuiper      ########   odam.nl         */
+/*   Created: 2022/02/11 15:02:47 by mikuiper      #+#    #+#                 */
+/*   Updated: 2022/02/11 16:47:50 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
 #include <stdio.h>
+#include <unistd.h>
+int	ft_error(char *s, int errno);
 
-int	main(void)
+typedef struct s_state
 {
-	printf("Hey");	
+	
+}				t_state;
+
+
+static int	ft_strlen(char *s)
+{
+	int	i;
+	
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+
+int	pipex_error(char *s)
+{
+	write(2, "Error\n", 6);
+	write(2, s, ft_strlen(s));
+	write(2, "\n", 1);
+	// free?
+	return (1);
+}
+
+int pipex_open_mode(char *filename, int mode)
+{
+	if (mode == MODE_INPUT)
+	{
+		//DO
+	}
+	else if (mode == MODE_OUTPUT)
+	{
+		//DO	
+	}
+	else
+		pipex_error("");
+}
+
+int	main(int argc, char **argv)
+{
+	int fd_input;
+	int fd_output;
+
+	if (argc != 5)
+		pipex_error("Invalid input. n arguments != 5\n");
+	fd_input = pipex_open_mode(argv[1], MODE_INPUT);
+	fd_output = pipex_open_mode(argv[4], MODE_OUTPUT);
+	
 	return (0);
+	
 }
