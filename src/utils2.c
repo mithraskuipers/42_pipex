f975b50e0e7d94dd11d5e5c7a5ab4e420ea8e671
Mithras Kuipers <mikuiper@f0r3s16.codam.nl>
2022-03-15 13:40:43 +0100

    update

--- src/utils2.c
+++ src/utils2.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/04 11:13:30 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/03/15 11:35:51 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/03/15 13:40:19 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	dst[i] = '\0';
 	return (ft_strlen(src));
 }
-
-/*
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	size_t	s3_len;
-	char	*s3;
-
-	if ((s1 == NULL) || (s2 == NULL))
-		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	s3_len = s1_len + 1 + s2_len + 1;
-	if (!(s3 = (char *)malloc(sizeof(char) * s3_len)))
-		return (NULL);
-	ft_strlcpy(s3, s1, s3_len);
-	ft_strlcpy((s3 + s1_len), s2, s3_len);
-	printf("%s", s3);
-	//free (s2);
-	return (s3);
-}
-*/
 
 char	*ft_strjoin(char *s1, char *s2)
 {
