710f7cec4e0cd4c7e15629cf0406b5090299a524
Mithras Kuipers <mkuipers@Mithrass-MacBook-Air.local>
2022-04-05 17:54:54 +0200

    added strndup

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/22 20:49:59 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/03/28 16:56:13 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/04/05 17:53:53 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,5 +86,6 @@ int		ft_strlen_fd(int fd);
 int		ft_strlen_char(char *s, int c);
 void	mk_putnbr_base(int nbr, int base);
 void	*ft_calloc_new(size_t count, size_t size);
+char	*ft_strndup(char *src, int len);
 
 #endif
