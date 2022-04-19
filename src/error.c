b1a4065a9ab3545fc3314cbb930f015a902c91f8
Mithras Kuipers <mikuiper@f0r1s14.codam.nl>
2022-04-19 11:11:45 +0200

    update

--- src/error.c
+++ src/error.c
@@ -6,13 +6,13 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/15 21:20:15 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/04/19 07:24:14 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/04/19 11:10:42 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	msg_cmd_error(char	*cmd, char *s, int fd)
+void	msg_cmd_error(char *cmd, char *s, int fd)
 {
 	ft_putstr_fd(cmd, fd);
 	ft_putstr_fd(s, fd);
